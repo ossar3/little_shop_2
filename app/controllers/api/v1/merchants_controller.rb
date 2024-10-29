b8c49fffd67caa32e85c0610cfcd9510b5b88762
Paul Knapp <ppankluap@gmail.com>
2024-10-29 12:00:46 -0500

    Feat: create merchants with json body returned.

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,4 +3,15 @@ class Api::V1::MerchantsController < ApplicationController
         merchant = Merchant.find(params[:id])
         render json: MerchantSerializer.new(merchant)
     end
+
+    def create
+        merchant =  Merchant.create(merchant_params)
+        render json: MerchantSerializer.new(merchant)
+    end
+end
+
+private
+
+def merchant_params
+    params.require(:merchant).permit(:name)
 end
