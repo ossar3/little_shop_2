558f42f27990a93f227cf1fe8e65cdce24e399fc
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-10-30 08:36:21 -0600

    added #update

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,4 +3,17 @@ class Api::V1::MerchantsController < ApplicationController
         merchant = Merchant.find(params[:id])
         render json: MerchantSerializer.new(merchant)
     end
+
+    def update
+        merchant = Merchant.find(params[:id])
+        if merchant.update(merchant_params)
+            render json: MerchantSerializer.new(merchant), status: :ok
+        end
+    end
+
+    private
+
+    def merchant_params
+        params.require(:merchant).permit(:name)
+    end
 end
