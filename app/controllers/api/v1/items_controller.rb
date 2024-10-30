f121c65e10a6cc0111eaa4d0cb6b5f3671d300a5
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-10-30 08:36:06 -0600

    added #update

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,17 @@ class Api::V1::ItemsController < ApplicationController
         item = Item.find(params[:id])
         render json: ItemSerializer.new(item)
     end
+
+    def update
+        item = Item.find(params[:id])
+        if item.update(item_params)
+            render json: ItemSerializer.new(item), status: :ok
+        end
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+    end
 end
