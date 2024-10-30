2dccbd6cc8883514e6cbf6944bc624d19c7c1f28
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-10-30 14:01:22 -0600

    commit for rounte config

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,5 @@ Rails.application.routes.draw do
   get "/api/v1/items/:id", to: "api/v1/items#show"
   delete "/api/v1/items/:id", to: "api/v1/items#destroy"
   post "/api/v1/items", to: "api/v1/items#create"
-  patch "/api/v1/merchants/:id", to: "api/v1/merchants#update"
+  put "/api/v1/items/:id", to: "api/v1/items#update"
 end
