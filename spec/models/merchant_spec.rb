ca489374b4bc183cfb519108d8d88f5a2b05d745
Terra Manning <terradmanning@gmail.com>
2024-10-30 14:54:29 -0600

    feat: add model tests for sort merchants by created at timestamp

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,4 +8,27 @@ RSpec.describe Merchant, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:name) }
   end
+
+  describe '.fetch_merchants' do
+    before(:each) do
+      @merchant_1 = Merchant.create!(name: "Test Merchant 1", created_at: 3.seconds.ago)
+      @merchant_2 = Merchant.create!(name: "Test Merchant 1", created_at: 2.seconds.ago)
+      @merchant_3 = Merchant.create!(name: "Test Merchant 3", created_at: 1.second.ago)
+    end
+
+    it "returns all merchants when no sorted parameter is provided" do
+      result = Merchant.fetch_merchants({})
+
+      expect(result.count).to eq(3)
+      expect(result).to contain_exactly(@merchant_1, @merchant_2, @merchant_3)
+    end
+
+    it "returns merchants sorted by age when sorted=age is provided" do
+      result = Merchant.fetch_merchants({ sorted: 'age' })
+
+      expect(result.count).to eq(3)
+      expect(result.first).to eq(@merchant_3) 
+      expect(result.last).to eq(@merchant_1)  
+    end
+  end
 end
