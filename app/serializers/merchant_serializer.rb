0084acaa54ce24d8edefbc2fb626a49e4b58eba3
Terra Manning <terradmanning@gmail.com>
2024-10-30 19:03:18 -0600

    fix: updates merchant serializer and index method for sorting params for all items by price

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,4 +1,8 @@
 class MerchantSerializer
   include JSONAPI::Serializer
   attributes :name
+
+  attribute :item_count, if: Proc.new { |_merchant, params| params && params[:sorted] == 'age' } do |merchant|
+    merchant.items.size
+  end
 end
