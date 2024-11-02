56d741eddf733ede4dd7c1ca7d543948ede3f862
Paul Knapp <ppankluap@gmail.com>
2024-11-02 12:14:21 -0500

    Feat: show all customers method.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,8 @@ class Customer < ApplicationRecord
   has_many :invoices
 
   validates :first_name, :last_name, presence: true
+
+  def self.show_all_customers(merchant_id)
+    Customer.joins(:invoices).where("invoices.merchant_id = ?", merchant_id ).distinct
+  end
 end
