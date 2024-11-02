e840e7e3f69217001c1f59dcb5322f22592067e8
Paul Knapp <ppankluap@gmail.com>
2024-11-02 12:13:53 -0500

    Feat: creat show customers for merchant method.

---
+++ app/controllers/api/v1/customers_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::CustomersController < ApplicationController  
+rescue_from ActiveRecord::RecordNotFound, with: :not_found_error_response
+
+    def show
+        customers = Customer.show_all_customers(params[:id])
+        render json: CustomerSerializer.new(customers)
+    end
+
+
+
+
+    private
+    
+    def not_found_error_response(error)
+        render json: ErrorSerializer.new(ErrorMessage.new(error.message, 404))
+        .serialize_json, status: :not_found
+    end
+end
