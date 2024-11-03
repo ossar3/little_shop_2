bfc9a213c3373c82f571ee0c6ac73e251864b9d5
Paul Knapp <ppankluap@gmail.com>
2024-11-03 09:10:12 -0600

    Test: tests for get all customers for a merchant responses created

---
+++ spec/requests/api/v1/merchant_customer_requests_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe "Merchants customer endpoints", type: :request do
+  before(:each) do 
+   @merchant1 = Merchant.create(name: "Test Merchant 1", created_at: 3.seconds.ago)
+   @merchant2 = Merchant.create(name: "Test Merchant 2", created_at: 2.seconds.ago)
+   @merchant3 = Merchant.create(name: "Test Merchant 3", created_at: 1.second.ago)
+
+   @customer_1 = Customer.create(first_name: "Abby", last_name: "Bronson")
+   @customer_2 = Customer.create(first_name: "Catherine", last_name: "Deitch")
+   @customer_3 = Customer.create(first_name: "Edgar", last_name: "Flores")
+   @customer_4 = Customer.create(first_name: "Gordan", last_name: "Herrowits")
+    
+   @invoice_1 = Invoice.create(customer_id: (@customer_1.id) , merchant_id: (@merchant1[:id]), status: "returned")
+   @invoice_2 = Invoice.create(customer_id: (@customer_3.id) , merchant_id: (@merchant2[:id]), status: "returned")
+   @invoice_3 = Invoice.create(customer_id: (@customer_4.id) , merchant_id: (@merchant3[:id]), status: "returned")
+   @invoice_4 = Invoice.create(customer_id: (@customer_1.id) , merchant_id: (@merchant1[:id]), status: "returned")
+   @invoice_4 = Invoice.create(customer_id: (@customer_1.id) , merchant_id: (@merchant2[:id]), status: "returned")
+ 
+  end
+
+  it "can send requests and recieve a response" do
+
+    get "/api/v1/merchants/#{@merchant1.id}/customers"
+    result_1 = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to have_http_status(200)
+    get "/api/v1/merchants/#{@merchant2.id}/customers"
+    expect(response).to have_http_status(200)
+    result_2 = JSON.parse(response.body, symbolize_names: true)
+    
+  end 
+
+  it "can send request and recieve a response sad path" do
+    
+  end
+end
