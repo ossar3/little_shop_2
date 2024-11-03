594c12445bbaeb2c49cff8e8c0b3276f39d7cef4
Paul Knapp <ppankluap@gmail.com>
2024-11-03 10:49:06 -0600

    Test: send requests for index tests full functionality.

--- spec/requests/api/v1/merchant_customer_requests_spec.rb
+++ spec/requests/api/v1/merchant_customer_requests_spec.rb
@@ -10,24 +10,26 @@ RSpec.describe "Merchants customer endpoints", type: :request do
    @customer_2 = Customer.create(first_name: "Catherine", last_name: "Deitch")
    @customer_3 = Customer.create(first_name: "Edgar", last_name: "Flores")
    @customer_4 = Customer.create(first_name: "Gordan", last_name: "Herrowits")
-    
-   @invoice_1 = Invoice.create(customer_id: (@customer_1.id) , merchant_id: (@merchant1[:id]), status: "returned")
-   @invoice_2 = Invoice.create(customer_id: (@customer_3.id) , merchant_id: (@merchant2[:id]), status: "returned")
-   @invoice_3 = Invoice.create(customer_id: (@customer_4.id) , merchant_id: (@merchant3[:id]), status: "returned")
-   @invoice_4 = Invoice.create(customer_id: (@customer_1.id) , merchant_id: (@merchant1[:id]), status: "returned")
-   @invoice_4 = Invoice.create(customer_id: (@customer_1.id) , merchant_id: (@merchant2[:id]), status: "returned")
+
+   @invoice_1 = Invoice.create(customer_id: (@customer_1[:id]) , merchant_id: (@merchant1[:id]), status: "returned")
+   @invoice_2 = Invoice.create(customer_id: (@customer_3[:id]) , merchant_id: (@merchant2[:id]), status: "returned")
+   @invoice_3 = Invoice.create(customer_id: (@customer_4[:id]) , merchant_id: (@merchant3[:id]), status: "returned")
+   @invoice_4 = Invoice.create(customer_id: (@customer_1[:id]) , merchant_id: (@merchant1[:id]), status: "returned")
+   @invoice_4 = Invoice.create(customer_id: (@customer_1[:id]) , merchant_id: (@merchant2[:id]), status: "returned")
  
   end
 
   it "can send requests and recieve a response" do
-
     get "/api/v1/merchants/#{@merchant1.id}/customers"
     result_1 = JSON.parse(response.body, symbolize_names: true)
     expect(response).to have_http_status(200)
     get "/api/v1/merchants/#{@merchant2.id}/customers"
     expect(response).to have_http_status(200)
     result_2 = JSON.parse(response.body, symbolize_names: true)
-    
+
+    expect(result_1[:data][0][:id]).to eq((@customer_1[:id]).to_s)
+    expect(result_2[:data][0][:id]).to eq((@customer_1[:id]).to_s)
+    expect(result_2[:data][1][:id]).to eq((@customer_3[:id]).to_s)
   end 
 
   it "can send request and recieve a response sad path" do
