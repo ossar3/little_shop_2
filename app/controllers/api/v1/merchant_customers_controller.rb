c3222b4b301bb55248a90ca96451ce41afb2db54
devklynch <devklynch@gmail.com>
2024-11-04 15:10:10 -0700

    merge fixes

--- app/controllers/api/v1/merchant_customers_controller.rb
+++ app/controllers/api/v1/merchant_customers_controller.rb
@@ -20,21 +20,3 @@ class Api::V1::MerchantCustomersController < ApplicationController
         end
     end
 end
-class Api::V1::MerchantCustomersController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :not_found_error_response
-
-    def index
-        customers = Customer.show_all_customers(params[:id])
-        render json: CustomerSerializer.new(customers)
-    end
-
-
-
-
-    private
-
-    def not_found_error_response(error)
-        render json: ErrorSerializer.new(ErrorMessage.new(error.message, 404))
-        .serialize_json, status: :not_found
-    end
-end
